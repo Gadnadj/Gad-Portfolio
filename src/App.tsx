b961c9455e9aae90263bfabe91672bfba902f5e2
Gad Nadjar <gadnadjar@gmail.com>
2025-01-26 16:48:17 +0200

    fix section

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
     <div className="overflow-x-hidden">
       <Headers />
       <Hero />
-      <SectionDivider bgColor="bg-secondary" />
       <About />
       <SectionDivider bgColor="bg-secondary" />
       <Skills />
