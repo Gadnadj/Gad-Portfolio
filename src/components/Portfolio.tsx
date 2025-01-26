ddb54f1272b02df49353867d827f98769ccff75a
Gad Nadjar <gadnadjar@gmail.com>
2025-01-26 17:00:40 +0200

    fix bug build

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,5 +1,4 @@
 import Projects from './Projects';
-import Reveal from './Reveal';
 import { motion } from 'framer-motion';
 import { useState } from 'react';
 import { projectsNav } from '../data';
